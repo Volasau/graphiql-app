94def515b96f8721f4cd6985f0883dd842ea3cfa
Ryhor Volsasu <g.volosov@email.com>
2023-12-08 14:53:29 +0400

    feat: add component ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './context/contextLanguage';
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
+      <ErrorBoundary>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
