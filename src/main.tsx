ed08839bd3bc61e1959f2b3b56fbcbf225864733
Azimkhan93 <aabdulsatarov@gmail.com>
2023-12-10 04:18:55 +0500

    feat: add firebase and userContextProvider to the root

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './context/contextLanguage';
 import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
+import { UserProvider } from './context/authContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ErrorBoundary>
         <LanguageProvider>
-          <App />
+          <UserProvider>
+            <App />
+          </UserProvider>
         </LanguageProvider>
       </ErrorBoundary>
     </BrowserRouter>
