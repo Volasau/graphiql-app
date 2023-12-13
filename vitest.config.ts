214c82ab0ec6fe5324fecff8ba7e19637e9dd840
Ryhor Volsasu <g.volosov@email.com>
2023-12-13 16:26:14 +0400

    feat: add test for Error page

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,19 +17,3 @@ export default mergeConfig(
     },
   })
 );
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig(() => ({
-//   base: './',
-//   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//     setupFiles: './src/setupTests.ts',
-//     coverage: {
-//       reporter: ['text', 'json', 'html'],
-//     },
-//   },
-// }));
