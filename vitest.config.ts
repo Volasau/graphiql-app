b5f3ff615ee2c7cd8e36f6caada9701f11bd2a94
Ryhor Volsasu <g.volosov@email.com>
2023-12-13 15:59:35 +0400

    feat: setup vitest for testing

---
+++ vitest.config.ts
@@ -0,0 +1,35 @@
+import { mergeConfig, defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+import react from '@vitejs/plugin-react';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      watch: false,
+      setupFiles: './src/setup.ts',
+      coverage: {
+        exclude: ['src/main.tsx', 'src/vite-env.d.ts', 'src/setup.ts'],
+      },
+    },
+  })
+);
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(() => ({
+//   base: './',
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/setupTests.ts',
+//     coverage: {
+//       reporter: ['text', 'json', 'html'],
+//     },
+//   },
+// }));
