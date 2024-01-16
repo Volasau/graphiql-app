2f246aa24be95a4a29f2a48e6cabfe02eec220e1
Ryhor Volsasu <g.volosov@email.com>
2024-01-16 19:03:22 +0400

    refactor: change condition for selecting text

--- src/components/AboutCourse/AboutCourse.tsx
+++ src/components/AboutCourse/AboutCourse.tsx
@@ -10,16 +10,10 @@ function AboutCourse() {
       <div data-testid="about_course" className={style.container}>
         <div className={style.wrapper}>
           <div className={style.text}>
-            <h3>{lan === 'en' ? 'About Course' : 'О курсе'}</h3>
-            <p className={style.title}>
-              {lan === 'en' ? DataAboutCourse.en.p1 : DataAboutCourse.ru.p1}
-            </p>
-            <p className={style.texte}>
-              {lan === 'en' ? DataAboutCourse.en.p2 : DataAboutCourse.ru.p2}
-            </p>
-            <p className={style.list}>
-              {lan === 'en' ? DataAboutCourse.en.p3 : DataAboutCourse.ru.p3}
-            </p>
+            <h3>{DataAboutCourse[lan].title}</h3>
+            <p className={style.title}>{DataAboutCourse[lan].p1}</p>
+            <p className={style.texte}>{DataAboutCourse[lan].p2}</p>
+            <p className={style.list}>{DataAboutCourse[lan].p3}</p>
           </div>
           <p className={style.logo__wrapper}>
             <img src={Rss} className={style.logorss} alt="logorss" />
